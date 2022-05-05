6177a57d96d78608a6ceb9c3aafb1f14a0306017
parseb <petra306@protonmail.com>
2022-05-06 01:27:19 +0300

    client next

--- client/components/Header.js
+++ client/components/Header.js
@@ -3,13 +3,20 @@ import { ConnectButton } from "web3uikit";
 
 export default function Header() {
     return(
-        <nav className="p-5 border-b-5 flex-row">
+        <div className="container">
+                    <nav className="p-5 border-b-2 flex flex-row">
             <h1 className="py-4 px-4 font-bold text-3xl">
-                Lite Vest
+                Little Vest
             </h1>
             <div className="ml-auto py-2 px-4">
-                <ConnectButton moralisAuth={false}/>
+                <ConnectButton moralisAuth={true}/>
             </div>
         </nav>
+
+        <div className="flex flex-row">
+            <h3>textttt abcdefghijklmnopq</h3>    
+        </div>
+        </div>
+
     )
 }
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -3,8 +3,14 @@ import { MoralisProvider } from 'react-moralis'
 
 
 function MyApp({ Component, pageProps }) {
+  
+
+  const appId = process.env.NEXT_PUBLIC_MORALIS_APPLICATION_ID
+  const serverUrl = process.env.NEXT_PUBLIC_MORALIS_SERVER_URL
+
+  
   return (
-  <MoralisProvider initializeOnMount={false}>
+  <MoralisProvider appId={appId} serverUrl={serverUrl}>
    <Component {...pageProps} />
   </MoralisProvider>
   )
