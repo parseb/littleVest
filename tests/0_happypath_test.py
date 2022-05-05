63766b9bbb2df6e083d095ca4b042ec9a6df2ad2
parseb <petra306@protonmail.com>
2022-05-05 12:43:20 +0300

    happy path continue

--- tests/0_happypath_test.py
+++ tests/0_happypath_test.py
@@ -17,6 +17,8 @@ def test_assume_check(LittleV, mock20_0, mock20_1):
 def test_happy_path(LittleV, mock20_0, mock20_1):
     a0 = {"from": accounts[0]}
     a1 = {"from": accounts[1]}
+    a2 = {"from": accounts[2]}
+    a3 = {"from": accounts[3]}
 
     a0_LV_balance = LittleV.balanceOf(accounts[0], a0)
     a1_LV_balance = LittleV.balanceOf(accounts[1], a1)
@@ -47,6 +49,8 @@ def test_happy_path(LittleV, mock20_0, mock20_1):
 
     mock20_1.transfer(accounts[0], 300 * 10 **18, a1)
     mock20_1.approve(LittleV.address, 99999*10**18, a0)
+
+    # token, beneficiary, ammount, days, sender
     assert LittleV.setVest(mock20_1.address, accounts[2].address, 100, 200, a0).return_value
 
     with reverts():
@@ -57,4 +61,50 @@ def test_happy_path(LittleV, mock20_0, mock20_1):
 
     assert LittleV.setVest(mock20_0.address, accounts[3].address, 100, 200, a1).return_value
 
-    #######
+    with reverts():
+        LittleV.withdrawAvailable(mock20_0.address, a1)
+
+    with reverts():
+        LittleV.withdrawAvailable(mock20_1.address, a0)
+
+    chain.mine(256)
+
+    # partial withdraw
+    chain.sleep(100 * 86400)
+    partial2 = LittleV.getVest(mock20_0.address, accounts[2].address)
+    assert partial2 > 999 * 10 ** 18
+    assert LittleV.withdrawAvailable(mock20_0.address, a2).return_value == True
+    withdrawn1 = LittleV.getVest(mock20_0.address, accounts[2].address)
+    assert partial2 > withdrawn1
+
+    # witdraw rest
+    chain.sleep(100 * 86400)
+    chain.mine(1)
+
+    assert LittleV.withdrawAvailable(mock20_0.address, a2).return_value == True
+    withdrawn1 = LittleV.getVest(mock20_0.address, accounts[2].address)
+    assert partial2 > withdrawn1
+    assert withdrawn1 == 0
+
+    
+    #### no partial withdrawal
+
+    partial2 = LittleV.getVest(mock20_1.address, accounts[3].address)
+    assert partial2 > 999 * 10 ** 18
+    assert LittleV.withdrawAvailable(mock20_1.address, a3).return_value == True
+    assert LittleV.balanceOf(accounts[3].address, a1) == 1
+    withdrawn1 = LittleV.getVest(mock20_1.address, accounts[3].address)
+    
+    assert partial2 > withdrawn1
+    assert withdrawn1 == 0
+
+
+    ### test nft transfer withdrawal
+
+    # create vest
+    assert LittleV.setVest(mock20_1.address, accounts[2].address, 100, 200, a0).return_value
+    #transfer nft
+
+    # receiver claims vest
+
+    # burn nft on full witdrawal.. override withdrawAvailable
