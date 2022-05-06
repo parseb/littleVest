36916a6b1b2d9fc80fba0665a5623536ea94b083
parseb <petra306@protonmail.com>
2022-05-06 13:38:27 +0300

    js frameworks are pure pain

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,21 +1,20 @@
 import { ConnectButton } from "web3uikit";
-
+import  Image  from "next/image";
 
 export default function Header() {
     return(
         <div className="container">
-                    <nav className="p-5 border-b-2 flex flex-row">
-            <h1 className="py-4 px-4 font-bold text-3xl">
-                Little Vest
-            </h1>
+        <nav className="p-5 border-b-2 flex flex-row">
+            
+            <div className="ml-auto py-2 float-left">
+                <img src="logo.png" alt="logo" className="h-20 float-left" />
+            </div>
+                
+       
             <div className="ml-auto py-2 px-4">
                 <ConnectButton moralisAuth={true}/>
             </div>
         </nav>
-
-        <div className="flex flex-row">
-            <h3>textttt abcdefghijklmnopq</h3>    
-        </div>
         </div>
 
     )
--- client/pages/index.js
+++ client/pages/index.js
@@ -2,12 +2,24 @@ import Head from 'next/head'
 
 import styles from '../styles/Home.module.css'
 import Header from '../components/Header'
+import {Row, Col}  from 'web3uikit'
+
 
 export default function Home() {
   return (
     <div className={styles.container}>
-        <Header />
-
+      
+      <Row>
+      <Header />
+      </Row>
+      
+      <Row>
+      <Col>
+        <h1>Hello Next.js</h1>
+      </Col>
+      </Row>
+       
+      
     </div>
   )
 }
